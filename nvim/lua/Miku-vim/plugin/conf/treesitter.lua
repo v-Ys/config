b49d140a45a245ddb60c292856bc5df6b6206ca9
Vv-ys <vincent.vv.mys@gmail.com>
2023-04-06 09:02:52 +0800

    delete unuse files

--- nvim/lua/Miku-vim/plugin/conf/treesitter.lua
+++ nvim/lua/Miku-vim/plugin/conf/treesitter.lua
@@ -1,30 +1,22 @@
 --NOTE:
 --fold
 require('ufo').setup({
-    provider_selector = function(bufnr, filetype, buftype)
-        return { 'treesitter', 'indent' }
-    end
+        provider_selector = function(bufnr, filetype, buftype)
+                return { 'treesitter', 'indent' }
+        end
 })
 
 
 require 'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all"
-    ensure_installed = { "c", "cpp" },
-    sync_install = false,
+        ensure_installed = { "c", "cpp", "lua" },
+        sync_install = false,
 
 
-    highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = true,
-    },
-    indent = {
-        enable = true
-    }
+        highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = true,
+        },
+        indent = {
+                enable = true
+        }
 }
