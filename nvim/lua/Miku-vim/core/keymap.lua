9d5a281eea5ca62955eb5026a88acd7f0cab8e94
Vv-ys <vincent.vv.mys@gmail.com>
2023-04-10 16:30:25 +0800

    map 'gb' as '%'

--- nvim/lua/Miku-vim/core/keymap.lua
+++ nvim/lua/Miku-vim/core/keymap.lua
@@ -22,54 +22,55 @@ local mode_adapters = {
 local keymaps = {
         insert_mode = {
 
-                    ["<A-s>"] = "<cmd>w<CR>",
+                ["<A-s>"] = "<cmd>w<CR>",
         },
         normal_mode = {
-                    ["<leader>m"] = "<cmd>lua require('Miku-vim.utils.modules').run()<cr>",
+                ["<leader>m"]  = "<cmd>lua require('Miku-vim.utils.modules').run()<cr>",
                 -- Resize with arrows
-                    ["<A-Up>"] = ":resize +2<CR>",
-                    ["<A-Down>"] = ":resize -2<CR>",
-                    ["<A-Left>"] = ":vertical resize -2<CR>",
-                    ["<A-Right>"] = ":vertical resize +2<CR>",
+                ["<A-Up>"]     = ":resize +2<CR>",
+                ["<A-Down>"]   = ":resize -2<CR>",
+                ["<A-Left>"]   = ":vertical resize -2<CR>",
+                ["<A-Right>"]  = ":vertical resize +2<CR>",
                 -- Move current line / block
                 -- ["<c-=>"] = ":m .+1<CR>==",
                 -- ["<c-->"] = ":m .-2<CR>==",
 
                 -- -- Better move
-                    ["K"]      = "3k",
-                    ["J"]      = "3j",
-                    ["H"]      = "^",
-                    ["L"]      = "$",
-                    ["<"]      = "<<",
-                    [">"]      = ">>",
-                    ["<leader>u"] = "J",
+                ["K"]          = "3k",
+                ["J"]          = "3j",
+                ["H"]          = "^",
+                ["L"]          = "$",
+                ["gb"]         = "%",
+                ["<"]          = "<<",
+                [">"]          = ">>",
+                ["<leader>u"]  = "J",
                 --buffers
-                    ["<A-]>"]  = ":BufferLineCycleNext<CR>",
-                    ["<A-[>"]  = ":BufferLineCyclePre<CR>",
-                    ["<c-]>"]  = ":bd<CR>",
+                ["<A-]>"]      = ":BufferLineCycleNext<CR>",
+                ["<A-[>"]      = ":BufferLineCyclePre<CR>",
+                ["<c-]>"]      = ":bd<CR>",
                 --cancel the highlight
-                    ["<leader>q"] = ":nohl<CR>",
+                ["<leader>q"]  = ":nohl<CR>",
                 --save and format
-                    ["<A-s>"]  = "<cmd>w<CR>",
+                ["<A-s>"]      = "<cmd>w<CR>",
                 --dashboad
-                    ["<Leader>ss"] = ":<C-u>SessionSave<CR>",
-                    ["<Leader>sl"] = ":<C-u>SessionLoad<CR>",
+                ["<Leader>ss"] = ":<C-u>SessionSave<CR>",
+                ["<Leader>sl"] = ":<C-u>SessionLoad<CR>",
                 --Telescope
-                    ["<leader>fh"] = "<cmd>lua require('telescope.builtin').oldfiles()<CR>",
-                    ["<leader>ff"] = "<cmd>lua require('telescope.builtin').find_files()<CR>",
-                    ["<leader>fw"] = "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>",
-                    ["<leader>fl"] = "<cmd>lua require('telescope.builtin').live_grep()<CR>",
-                    ["<leader>fb"] = "<cmd>lua require('telescope.builtin').buffers()<CR>",
-                    ["<Leader>fm"] = "<cmd>lua require('telescope.builtin').marks()<CR>",
-                    ["<Leader>fs"] = "<cmd>lua require('telescope.builtin').colorscheme()<CR>",
-                    ["<Leader>fp"] = "<cmd>lua require'telescope'.extensions.project.project{}<CR>",
-                    ["<Leader>fr"] = "<cmd>lua require('telescope.builtin').commands()<CR>",
-                    ["<Leader>fM"] = "<cmd>TodoTelescope<CR>",
+                ["<leader>fh"] = "<cmd>lua require('telescope.builtin').oldfiles()<CR>",
+                ["<leader>ff"] = "<cmd>lua require('telescope.builtin').find_files()<CR>",
+                ["<leader>fw"] = "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>",
+                ["<leader>fl"] = "<cmd>lua require('telescope.builtin').live_grep()<CR>",
+                ["<leader>fb"] = "<cmd>lua require('telescope.builtin').buffers()<CR>",
+                ["<Leader>fm"] = "<cmd>lua require('telescope.builtin').marks()<CR>",
+                ["<Leader>fs"] = "<cmd>lua require('telescope.builtin').colorscheme()<CR>",
+                ["<Leader>fp"] = "<cmd>lua require'telescope'.extensions.project.project{}<CR>",
+                ["<Leader>fr"] = "<cmd>lua require('telescope.builtin').commands()<CR>",
+                ["<Leader>fM"] = "<cmd>TodoTelescope<CR>",
                 --leap
-                    ["f"]      = "<Plug>(leap-forward-to)",
-                    ["gf"]     = "<Plug>(leap-backward-to)",
+                ["f"]          = "<Plug>(leap-forward-to)",
+                ["gf"]         = "<Plug>(leap-backward-to)",
                 --n vim tree
-                    ["<leader>n"] = "<cmd>NeoTreeFloatToggle<CR>",
+                ["<leader>n"]  = "<cmd>NeoTreeFloatToggle<CR>",
         },
         term_mode = {
                 -- Terminal window navigation
@@ -77,26 +78,26 @@ local keymaps = {
                 -- ["<C-j>"] = "<C-\\><C-N><C-w>j",
                 -- ["<C-k>"] = "<C-\\><C-N><C-w>k",
                 -- ["<C-l>"] = "<C-\\><C-N><C-w>l",
-                    ["<C-]>"] = "<C-\\><C-N>",
+                ["<C-]>"] = "<C-\\><C-N>",
         },
         visual_mode = {
-                    ["<A-s>"] = "<cmd>w<CR>",
+                ["<A-s>"] = "<cmd>w<CR>",
                 -- Better indenting
-                    ["<"] = "<gv",
-                    [">"] = ">gv",
-                    ["K"] = "3k",
-                    ["J"] = "3j",
-                    ["H"] = "^",
-                    ["L"] = "$",
+                ["<"] = "<gv",
+                [">"] = ">gv",
+                ["K"] = "3k",
+                ["J"] = "3j",
+                ["H"] = "^",
+                ["L"] = "$",
         },
         visual_block_mode = {
                 -- Move current line / block with Alt-j/k ala vscode.
-                    ["<A-s>"] = ":m '>+1<CR>gv-gv",
-                    ["<A-w>"] = ":m '<-2<CR>gv-gv",
-                    ["k"] = "3k",
-                    ["J"] = "3j",
-                    ["H"] = "^",
-                    ["L"] = "$",
+                ["<A-s>"] = ":m '>+1<CR>gv-gv",
+                ["<A-w>"] = ":m '<-2<CR>gv-gv",
+                ["k"] = "3k",
+                ["J"] = "3j",
+                ["H"] = "^",
+                ["L"] = "$",
         },
         command_mode = {
                 -- [";;"] = "lua require('Miku-vim.utils.modules').btop()",
@@ -105,7 +106,7 @@ local keymaps = {
 
 --NOTE: user command
 local command = {
-            ["FormatJSON"] = "%!python -m json.tool",
+        ["FormatJSON"] = "%!python -m json.tool",
 }
 
 --NOTE: load
