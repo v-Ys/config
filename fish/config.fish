87c0bde8c778a630a1da9c531377f21511046e85
Vv-ys <vincent.vv.mys@gmail.com>
2023-05-03 16:00:27 +0800

    proxy fun

--- fish/config.fish
+++ fish/config.fish
@@ -43,6 +43,20 @@ if status is-interactive
     #NOTE: clashx
 
     export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+    # function proxy_on
+    #     export   https_proxy=http://127.0.0.1:7890 
+    #     export   http_proxy=http://127.0.0.1:7890 
+    #     export   all_proxy=socks5://127.0.0.1:7890
+    #     echo -e "终端代理已开启。"
+    # end
+    #
+    # function proxy_off
+    #     export   https_proxy=
+    #     export   http_proxy=
+    #     export   all_proxy=
+    #     echo -e "终端代理已关闭。"
+    # end
+
 
 
     # NOTE: key bind
