e401c97ee802506eb932232f368e1f082f2bf8d5
Vv-ys <vincent.vv.mys@gmail.com>
2023-06-02 20:10:11 +0800

    change lsp diagnostic icon

--- nvim/lua/Miku-vim/plugin/conf/lsp/handlers.lua
+++ nvim/lua/Miku-vim/plugin/conf/lsp/handlers.lua
@@ -14,8 +14,8 @@ M.setup = function()
                 { name = "DiagnosticSignError", text = "" },
                 { name = "DiagnosticSignWarn",  text = "" },
                 { name = "DiagnosticSignHint",  text = "" },
-                { name = "DiagnosticSignInfo",  text = "ﲤ" },
-                ---         ✎ ﲤ     ▎ ▏ │       礼   
+                { name = "DiagnosticSignInfo",  text = "✎" },
+                -- -          ✎  ﲤ   ▎ ▏ │          
         }
 
         for _, sign in ipairs(signs) do
@@ -59,10 +59,10 @@ local function lsp_keymaps(bufnr)
         -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
         -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
         -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-        -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gp", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
         -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
         -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>', opts)
         -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "gt", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 
         vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
         vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>", opts)
@@ -84,10 +84,6 @@ M.on_attach = function(client, bufnr)
                 client.server_capabilities.document_formatting = false
         end
         lsp_keymaps(bufnr)
-        -- require "lsp_signature".on_attach({
-        --         hint_prefix = "🐼 ",
-        --         floating_window = false,
-        -- }, bufnr)
 end
 
 
