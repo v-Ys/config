cde45ff7a15bd699457a87f131b8d16e344b34b9
Vv-ys <vincent.vv.mys@gmail.com>
2023-06-07 15:15:11 +0800

    update

---
+++ nvim/lua/Miku-vim/core/lazy.lua
@@ -0,0 +1,63 @@
+vim.g.mapleader = "<Space>"
+vim.g.maplocalleader = "<Space>"
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system({
+                "git",
+                "clone",
+                "--filter=blob:none",
+                "https://github.com/folke/lazy.nvim.git",
+                "--branch=stable",
+                lazypath,
+        })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+        spec = {
+                { import = "Miku-vim.plugin.plugins" },
+        },
+        ui = {
+                size = { width = 0.8, height = 0.8 },
+                wrap = true, -- wrap the lines in the ui
+                border = "rounded",
+        },
+        install = {
+                colorscheme = { 'github_light', 'nightfox', },
+        },
+        performance = {
+                rtp = {
+                        disabled_plugins = {
+                                "2html_plugin",
+                                "tohtml",
+                                "getscript",
+                                "getscriptPlugin",
+                                "gzip",
+                                "logipat",
+                                "netrw",
+                                "netrwPlugin",
+                                "netrwSettings",
+                                "netrwFileHandlers",
+                                "matchit",
+                                "tar",
+                                "tarPlugin",
+                                "rrhelper",
+                                "spellfile_plugin",
+                                "vimball",
+                                "vimballPlugin",
+                                "zip",
+                                "zipPlugin",
+                                "tutor",
+                                "rplugin",
+                                "syntax",
+                                "synmenu",
+                                "optwin",
+                                "compiler",
+                                "bugreport",
+                                "ftplugin",
+                        },
+                },
+        },
+})
