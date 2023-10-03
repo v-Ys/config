f260211efe3cb2aea86f9e7784240deae73f76cd
Vv-ys <vincent.vv.mys@gmail.com>
2023-10-03 21:08:54 +0800

    format

--- nvim/lua/Miku-vim/core/autocmd.lua
+++ nvim/lua/Miku-vim/core/autocmd.lua
@@ -5,9 +5,17 @@
 --augroup end
 --]])
 
+local lang = "*.h," ..
+    "*.c," ..
+    "*.cpp," ..
+    "*.rs," ..
+    "*.go," ..
+    "*.py," ..
+    "*.lua,"
+
 local Format = vim.api.nvim_create_augroup("_format", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.c,*.py,*.cpp,*.rs,*.lua,*.vue,*.ts",
+        pattern = lang,
         command = "lua vim.lsp.buf.format{sync=true}",
         group = Format,
 })
