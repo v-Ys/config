29b14a2b9fe107ee949240c583317115b66f4f8c
Vv-ys <vincent.vv.mys@gmail.com>
2023-10-22 13:06:56 +0800

    feat:/.gitignore,add hammerspoon/Spoons

---
+++ hammerspoon/app.lua
@@ -0,0 +1,96 @@
+local M = {}
+local application = require 'hs.application'
+local alert = require 'hs.alert'
+local timer = require 'hs.timer'
+
+local function getBundleID(appName)
+        local script = 'osascript -e \'id of app "' .. appName .. '"\''
+        local handle = io.popen(script)
+        if handle ~= nil then
+                local result = handle:read("*a")
+                handle:close()
+                return string.gsub(result, "\n", "")
+        end
+end
+
+local function sleep(appName)
+        local appToSleep = {
+                ["Alacritty"] = "0.5",
+                ["WezTerm"] = "0.3",
+                ["Visual Studio Code"] = "0.6",
+        }
+        if appToSleep[appName] == nil then
+                return
+        end
+        os.execute("sleep " .. appToSleep[appName])
+end
+
+
+
+
+M.lanuchOrFocusApp = function(appName)
+        return function()
+                local appBundleID = getBundleID(appName)
+                if not appBundleID then
+                        return
+                end
+
+                local appsIsRunning = application.applicationsForBundleID(appBundleID)
+                local appIsRunning = appsIsRunning and appsIsRunning[1]
+
+                -- lanuch app and set full screen
+                if not appIsRunning then
+                        local runApp = application.launchOrFocusByBundleID(appBundleID)
+                        if runApp == false then
+                                alert('Failed to launch app: ' .. appName)
+                        else
+                                -- for fullScreen when first lanuch
+                                local checkTimerTimeoutCounter = 0
+                                CheckTimer = timer.new(0.1, function()
+                                        if checkTimerTimeoutCounter == 500 then
+                                                CheckTimer:stop()
+                                                checkTimerTimeoutCounter = 0
+                                                return
+                                        end
+
+                                        local appsFirstLanuch = application.applicationsForBundleID(appBundleID)
+                                        local appFirstLanuch = appsFirstLanuch and appsFirstLanuch[1]
+                                        if appFirstLanuch then
+                                                sleep(appName)
+                                                if appFirstLanuch:activate() then
+                                                        local focusedWindow = appFirstLanuch:focusedWindow()
+                                                        if focusedWindow then
+                                                                focusedWindow:setFullScreen(true)
+                                                                CheckTimer:stop()
+                                                                checkTimerTimeoutCounter = 0
+                                                                return
+                                                        end
+                                                end
+                                        end
+                                        checkTimerTimeoutCounter = checkTimerTimeoutCounter + 1
+                                end)
+                                CheckTimer:start()
+                                return
+                        end
+                end
+
+                -- focus app
+                local MainWin = appIsRunning:mainWindow()
+                if MainWin then
+                        if MainWin:isFullScreen() == true then
+                                MainWin:application():activate(true)
+                                MainWin:focus()
+                        elseif appIsRunning:isFrontmost() == true then
+                                MainWin:application():hide()
+                        else
+                                MainWin:application():activate(true)
+                                MainWin:application():unhide()
+                                MainWin:focus()
+                        end
+                else
+                        application.launchOrFocusByBundleID(appBundleID)
+                end
+        end
+end
+
+return M
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,94 +1,17 @@
--- define all key bindings here
 local hk = require "hs.hotkey"
-local hyper = { 'ctrl', 'alt', 'cmd', 'shift' }
-local hyper_error = { 'ctrl', 'cmd', 'shift' }
-local window = require "hs.window"
-local application = require 'hs.application'
-local alert = require 'hs.alert'
-
-local function sizeFocusedWindow(mode)
-        return function()
-                local win = window.focusedWindow()
-                local f = win:frame()
-                local screen = win:screen()
-                local max = screen:frame()
-                -- if mode == "FullScreen" then
-                --         hs.grid.maximizeWindow()
-                --         return
-                -- end
-
-                local size = {
-                        Max = { x = max.x, y = max.y, w = max.w, h = max.h },
-                        Right_Half = { x = max.x + max.w / 2, y = max.y, w = max.w / 2, h = max.h },
-                        Left_Half = { x = max.x, y = max.y, w = max.w / 2, h = max.h },
-                        Center = { x = max.x + max.w / 5, y = max.y + max.h / 6, w = max.w / 5 * 3, h = max.h / 3 * 2 },
-                }
-
-                f = size[mode]
-                win:setFrame(f, 0)
-        end
-end
-
-local function launch_or_toggle_application(_app)
-        -- finds a running applications
-        local app = application.find(_app)
-
-        if not app then
-                -- application not running, launch app
-                if false == application.launchOrFocus(_app) then
-                        -- launch failed, send notification
-                        alert('Failed to launch app: ' .. _app)
-                end
-                return
-        end
+local lanuchOrFocusApp = require("app").lanuchOrFocusApp
+local windowManager = require('window').windowManager
 
 
-        local mainwin = app:mainWindow()
-        if mainwin then
-                if true == app:isFrontmost() then
-                        mainwin:application():hide()
-                else
-                        mainwin:application():activate(true)
-                        mainwin:application():unhide()
-                        mainwin:focus()
-                end
-        else
-                -- no window, launchOrFocus
-                application.launchOrFocus(_app)
-        end
-end
+hk.bind({ 'ctrl', 'alt', 'cmd', 'shift' }, "q", windowManager("fullScreen"))
+hk.bind({ 'ctrl', 'alt', 'cmd', 'shift' }, "d", windowManager("right"))
+hk.bind({ 'ctrl', 'alt', 'cmd', 'shift' }, "a", windowManager("left"))
+hk.bind({ 'ctrl', 'alt', 'cmd', 'shift' }, "w", windowManager("top"))
+hk.bind({ 'ctrl', 'alt', 'cmd', 'shift' }, "s", windowManager("bottom"))
 
-local key2App = {
-        n = 'Google Chrome',
-        u = 'Safari',
-        i = 'Alacritty',
-        o = 'Craft',
-        l = 'WezTerm',
-        -- j = 'Google Chrome',
-        j = 'Arc.app',
-}
-
-for key, ap in pairs(key2App) do
-        hk.bind(hyper, key, function() launch_or_toggle_application(ap) end)
-end
-
-local App = {
-        -- down = 'Google Chrome',
-        down = 'Arc.app',
-        right = 'WezTerm',
-}
-
-for key, ap in pairs(App) do
-        hk.bind(hyper_error, key, function() launch_or_toggle_application(ap) end)
-end
-
-hk.bind(hyper, "d", sizeFocusedWindow("Right_Half"))
-hk.bind(hyper, "w", sizeFocusedWindow("Max"))
-hk.bind(hyper, "a", sizeFocusedWindow("Left_Half"))
-hk.bind(hyper, "s", sizeFocusedWindow("Center"))
-hk.bind(hyper, "q", function()
-        local win = window.focusedWindow()
-        if win ~= nil then
-                win:setFullScreen(not win:isFullScreen())
-        end
-end)
+hk.bind({ 'ctrl', 'alt', 'cmd', 'shift' }, "u", lanuchOrFocusApp("Safari"))
+hk.bind({ 'ctrl', 'alt', 'cmd', 'shift' }, "i", lanuchOrFocusApp("Alacritty"))
+hk.bind({ 'ctrl', 'alt', 'cmd', 'shift' }, "o", lanuchOrFocusApp("Craft"))
+hk.bind({ 'ctrl', 'alt', 'cmd', 'shift' }, "n", lanuchOrFocusApp("Visual Studio Code"))
+hk.bind({ 'ctrl', 'cmd', 'shift' }, "right", lanuchOrFocusApp("WezTerm"))
+hk.bind({ 'ctrl', 'cmd', 'shift' }, "down", lanuchOrFocusApp("Arc"))
