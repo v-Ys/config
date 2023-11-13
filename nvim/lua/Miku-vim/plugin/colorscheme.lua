1a200d1c7c1691e731c5cb412ef0c05353adc4fb
Vv-ys <vincent.vv.mys@gmail.com>
2023-11-13 23:05:12 +0800

    add new colorscheme monokai

--- nvim/lua/Miku-vim/plugin/colorscheme.lua
+++ nvim/lua/Miku-vim/plugin/colorscheme.lua
@@ -1,35 +1,3 @@
-local theme = {
-        light = {
-                {
-                        "projekt0n/github-nvim-theme",
-                        opts = {},
-                        config = function()
-                                vim.opt.bg = 'light'
-                                vim.cmd.colorscheme "github_light"
-                        end,
-                },
-                {
-
-                        "EdenEast/nightfox.nvim",
-                        event = "VeryLazy",
-                }
-        },
-        dark = {
-                {
-                        "EdenEast/nightfox.nvim",
-                        config = function()
-                                vim.opt.bg = 'dark'
-                                vim.cmd.colorscheme "duskfox"
-                        end,
-                },
-                {
-
-                        "projekt0n/github-nvim-theme",
-                        event = "VeryLazy",
-                },
-        }
-}
-
 return {
 
         {
@@ -37,13 +5,20 @@ return {
                 event = "VeryLazy",
         },
         {
-                "catppuccin/nvim",
-                name = "catppuccin",
+                "rose-pine/neovim",
+                name = 'rose-pine'
+        },
+        {
+
+                "EdenEast/nightfox.nvim",
                 event = "VeryLazy",
         },
         {
-                'rose-pine/neovim',
-                name = 'rose-pine'
-        },
-        theme.light,
+                "projekt0n/github-nvim-theme",
+                opts = {},
+                config = function()
+                        -- vim.cmd.colorscheme "github_light"
+                        vim.cmd.colorscheme "monokai"
+                end,
+        }
 }
