8118ee15f8b91df71598d6be55a4f9a6d2b1af89
Vv-ys <vincent.vv.mys@gmail.com>
2023-11-15 18:39:27 +0800

    use github theme

--- nvim/lua/Miku-vim/plugin/colorscheme.lua
+++ nvim/lua/Miku-vim/plugin/colorscheme.lua
@@ -1,28 +1,16 @@
 return {
-        {
-                "rose-pine/neovim",
-                name = 'rose-pine',
-                event = "VeryLazy",
-        },
-        {
-
-                "EdenEast/nightfox.nvim",
-                event = "VeryLazy",
-        },
-        {
-                "projekt0n/github-nvim-theme",
-                config = function()
-                        require('github-theme').setup({
-                                groups = {
-                                        -- all = {
-                                        --         Pmenu = { bg = 'bg0', fg = 'bg0' },
-                                        --         CmpDocumentationBorder = { fg = 'bg0', bg = 'bg0', },
-                                        --         CmpDocumentation = { fg = 'bg0', bg = 'bg0', },
-                                        -- },
-                                }
-                        })
-                        -- vim.cmd.colorscheme "github_light"
-                        vim.cmd.colorscheme "github_dark"
-                end,
-        },
+        "projekt0n/github-nvim-theme",
+        config = function()
+                require('github-theme').setup({
+                        groups = {
+                                -- all = {
+                                --         Pmenu = { bg = 'bg0', fg = 'bg0' },
+                                --         CmpDocumentationBorder = { fg = 'bg0', bg = 'bg0', },
+                                --         CmpDocumentation = { fg = 'bg0', bg = 'bg0', },
+                                -- },
+                        }
+                })
+                -- vim.cmd.colorscheme "github_light"
+                vim.cmd.colorscheme "github_dark"
+        end,
 }
