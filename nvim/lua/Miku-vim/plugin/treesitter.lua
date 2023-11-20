88ab644eea64d6cba56914302bd5aad69388b6b3
Vv-ys <vincent.vv.mys@gmail.com>
2023-11-20 10:41:43 +0800

    fix bug

--- nvim/lua/Miku-vim/plugin/treesitter.lua
+++ nvim/lua/Miku-vim/plugin/treesitter.lua
@@ -8,7 +8,6 @@ return {
                         sync_install = false,
                         highlight = {
                                 enable = true,
-                                -- additional_vim_regex_highlighting = true,
                         },
                         indent = {
                                 enable = true,
