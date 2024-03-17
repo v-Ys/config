15d2f02d0acd4b12ae9cac34e63f4b38d625302e
Vv-ys <vincent.vv.mys@gmail.com>
2024-03-17 12:58:19 +0800

    fix

--- nvim/lua/Miku-vim/core/options.lua
+++ nvim/lua/Miku-vim/core/options.lua
@@ -67,11 +67,7 @@ local signs = {
         { name = "DiagnosticSignWarn", text = "▎" },
         { name = "DiagnosticSignHint", text = "▎" },
         { name = "DiagnosticSignInfo", text = "▎" },
-<<<<<<< HEAD
         --   󰌕      ✎   ▎ ▏ │     
-=======
-        -- -   󰌕      ✎      ▎ ▏ │          
->>>>>>> developing
 }
 for _, sign in ipairs(signs) do
         vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
