44593aec8dfc9b10a9f977864533c8071e69ad29
Ys <vincent.vv.mys@gmail.com>
2024-05-07 14:50:32 +0800

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -73,7 +73,7 @@ local keybinds = {
                 },
         },
         {
-                key    = 'n',
+                key    = ']',
                 mods   = 'ALT',
                 action = act.ActivatePaneDirection 'Next',
         },
